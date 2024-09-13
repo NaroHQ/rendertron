c56b6b51440f9a0c2b1d1927e727da44cd5dcee9
Augusto Ody <odyaugusto@gmail.com>
2024-09-13 19:00:13 -0300

    Try another fix

--- render-build.sh
+++ render-build.sh
@@ -5,7 +5,7 @@ npm install
 npx playwright install chromium
 
 # Store/pull Playwright cache with build cache
-if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then 
+if [ ! -d $PLAYWRIGHT_BROWSERS_PATH ]; then 
   echo "...Copying Playwright Cache from Build Cache" 
   cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
 else 
